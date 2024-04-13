829e426440a60dde1ac9e43301bb1b21b151b9cf
Niskarsh <niskarshk@gmail.com>
2024-04-13 21:00:38 +0530

    Added code to find overlapping times

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,22 @@
 #![allow(non_snake_case)]
 
-pub fn findOverlappingTimes(timeset1: &Vec<Vec<i32>>, timeset2: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let overlappingTimes: Vec<Vec<i32>> = vec![vec![]];
+pub fn findOverlappingTimes(mut timeset1: Vec<Vec<i32>>, mut timeset2: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut overlappingTimes: Vec<Vec<i32>> = vec![];
+    timeset1.sort_by(|a, b| a[0].cmp(&b[0]));
+    timeset2.sort_by(|a, b| a[0].cmp(&b[0]));
+    let mut counter1 = 0;
+    let mut counter2 = 0;
+    while counter1 < timeset1.len() && counter2 < timeset2.len() {
+        let start = if timeset1[counter1][0] > timeset2[counter2][0] { timeset1[counter1][0] } else { timeset2[counter2][0] };
+        let end = if timeset1[counter1][1] < timeset2[counter2][1] { timeset1[counter1][1] } else { timeset2[counter2][1] };
+        if timeset1[counter1][1] < timeset2[counter2][1] {
+            counter1 += 1;
+        } else {
+            counter2 += 1;
+        }
+        if start < end {
+            overlappingTimes.push(vec![start, end]);
+        }
+    }
     overlappingTimes
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use find_overlapping_times::findOverlappingTimes;
 fn main() {
     let secTimeSet1 = vec![vec![1, 3], vec![13, 15], vec![16,20]];
     let secTimeSet2 = vec![vec![2, 14]];
-    let overlappingTimes = findOverlappingTimes(&secTimeSet1, &secTimeSet2);
+    println!("Time 1: {:?} \nTime 2: {:?}", secTimeSet1, secTimeSet2);
+    let overlappingTimes = findOverlappingTimes(secTimeSet1.clone(), secTimeSet2.clone());
     println!("Overlapping times: {:?}", overlappingTimes);
 }
